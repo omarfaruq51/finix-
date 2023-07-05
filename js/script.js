56f71c28bd1bb2729fab82a25f89979989c809ac
Omar Faruq <124316447+omarfaruq51@users.noreply.github.com>
2023-07-05 13:25:13 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,153 @@
+	// Partner Slider
+	$('.business-partner-slider').owlCarousel({
+		loop: true,
+		nav: false,
+		dots: false,
+		smartSpeed: 2000,
+		margin: 30,
+		autoplayHoverPause: true,
+		autoplay: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			1024: {
+				items: 4
+			},
+			1200: {
+				items: 5
+			}
+		}
+	});
+
+	// Best Services Slider JS
+	$(document).ready(function() {
+		var bigimage = $("#best-services");
+		var thumbs = $("#thumbs");
+		//var totalslides = 10;
+		var syncedSecondary = true;
+		
+		bigimage
+			.owlCarousel({
+			items: 1,
+			slideSpeed: 2000,
+			nav: true,
+			autoplay: true,
+			dots: false,
+			nav: false,
+			loop: true,
+			responsiveRefreshRate: 200,
+			navText: [
+				'<i class="fa-solid fa-arrow-left"></i>',
+				'<i class="fa-solid fa-arrow-right"></i>'
+			]
+		})
+			.on("changed.owl.carousel", syncPosition);
+		
+		thumbs
+			.on("initialized.owl.carousel", function() {
+			thumbs
+			.find(".owl-item")
+			.eq(0)
+			.addClass("current");
+		})
+		.owlCarousel({
+			loop: false,
+			dots: false,
+			nav: true,
+			autoplay: false,
+			active: true,
+			navText: [
+			'<i class="fa-solid fa-arrow-left"></i>',
+			'<i class="fa-solid fa-arrow-right"></i>'
+			],
+			smartSpeed: 200,
+			slideSpeed: 500,
+			slideBy: 8,
+			responsiveRefreshRate: 100,
+			responsive: {
+				0: {
+					items: 2
+				},
+				1024: {
+					items: 4
+				},
+				1200: {
+					items: 6
+				}
+			}
+		})
+		.on("changed.owl.carousel", syncPosition2);
+		
+		function syncPosition(el) {
+		//if loop is set to false, then you have to uncomment the next line
+		//var current = el.item.index;
+		
+		//to disable loop, comment this block
+			var count = el.item.count - 1;
+			var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+		
+			if (current < 0) {
+			current = count;
+			}
+			if (current > count) {
+			current = 0;
+			}
+			//to this
+			thumbs
+			.find(".owl-item")
+			.removeClass("current")
+			.eq(current)
+			.addClass("current");
+			var onscreen = thumbs.find(".owl-item.active").length - 1;
+			var start = thumbs
+			.find(".owl-item.active")
+			.first()
+			.index();
+			var end = thumbs
+			.find(".owl-item.active")
+			.last()
+			.index();
+		
+			if (current > end) {
+			thumbs.data("owl.carousel").to(current, 100, true);
+			}
+			if (current < start) {
+			thumbs.data("owl.carousel").to(current - onscreen, 100, true);
+			}
+		}
+		
+		function syncPosition2(el) {
+			if (syncedSecondary) {
+			var number = el.item.index;
+			bigimage.data("owl.carousel").to(number, 100, true);
+			}
+		}
+		
+		thumbs.on("click", ".owl-item", function(e) {
+			e.preventDefault();
+			var number = $(this).index();
+			bigimage.data("owl.carousel").to(number, 300, true);
+		});
+	});
+
+		// Clients Slider
+		$(document).ready(function() {
+		$('.clients-slider').owlCarousel({
+			loop: true,
+			nav: false,
+			dots: true,
+			smartSpeed: 2000,
+			margin: 30,
+			autoplayHoverPause: true,
+			autoplay: true,
+			items: 1,
+		});
+
+	});
